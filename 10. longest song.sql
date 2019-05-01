f7572ee3368a0df983139b45c6742ffb89bed047
Kimmy Bird <32465525+kimberly-bird@users.noreply.github.com>
2019-04-30 19:40:08 -0500

    Update 10. longest song.sql

--- 10. longest song.sql
+++ 10. longest song.sql
@@ -1,3 +1,11 @@
+---- SQLite ----
 SELECT MAX(SongLength) as "Longest Song", Song.Title as "Song Title", Album.Title as "Album Title"
 FROM Song, Album
 where song.AlbumId = Album.albumId
+
+---- SQL Server ----
+SELECT 
+  Title,
+  SongLength
+FROM Song 
+WHERE SongLength = (SELECT MAX(SongLength) FROM Song)
