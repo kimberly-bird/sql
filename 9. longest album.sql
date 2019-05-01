3ff959ee51e620bede198c12e087758173264f19
Kimmy Bird <32465525+kimberly-bird@users.noreply.github.com>
2019-04-30 19:37:27 -0500

    Update 9. longest album.sql

--- 9. longest album.sql
+++ 9. longest album.sql
@@ -1,2 +1,10 @@
+-------- SQLite --------
 SELECT MAX(AlbumLength) as "Longest Album", Album.Title
 FROM Album
+
+-------- SQL SERVER --------
+SELECT 
+  Title,
+  AlbumLength
+FROM Album
+WHERE AlbumLength = (SELECT MAX(AlbumLength) FROM Album)
